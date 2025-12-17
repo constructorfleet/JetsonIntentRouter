d5475462421372fba2c0dd16fd9db308856b8aa6
Teagan42 <that@teagantotally.rocks>
2025-12-16 20:04:52 -0700

    formatting

--- data/load_templates.py
+++ data/load_templates.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
 import json
-from os import path, walk
-from typing import TypeVar, Generic, Union
-import yaml
 import traceback
 from functools import lru_cache
 from itertools import product
+from os import path, walk
+from typing import Generic, TypeVar, Union
+
+import yaml
+
 
 def to_dict(obj) -> dict:
     if isinstance(obj, dict):
@@ -22,13 +24,14 @@ def to_dict(obj) -> dict:
     else:
         return obj
 
+
 class TemplateBase:
     def __init__(self, file_path: str = ""):
         self.file_path = file_path
-        
+
     def to_dict(self) -> dict:
         return to_dict(self)
-        
+
 
 TTemplate = TypeVar("TTemplate", bound=TemplateBase)
 TTemplateOrDict = TypeVar("TTemplateOrDict", bound=Union[TemplateBase, dict])
@@ -39,51 +42,52 @@ class ListTemplate(TemplateBase):
         super().__init__(file_path)
         self.id = id
 
+
 class ValueListTemplate(ListTemplate):
-    def __init__(self, id, file_path = "", values: list[str | int] = []):
+    def __init__(self, id, file_path="", values: list[str | int] = []):
         super().__init__(id, file_path)
         self.values = values
-    
+
 
 class Lists:
     def __init__(self, lists: dict[str, ListTemplate] = {}):
         self.lists = lists
-    
+
 
 class ExpansionRuleTemplate(TemplateBase):
     def __init__(self, id: str, file_path: str = "", rule: str = ""):
         super().__init__(file_path)
         self.id = id
         self.rule = rule
-    
+
 
 class ExpansionRules(TemplateBase):
-    def __init__(self, file_path = "", rules: dict[str, ExpansionRuleTemplate] = {}):
+    def __init__(self, file_path="", rules: dict[str, ExpansionRuleTemplate] = {}):
         super().__init__(file_path)
         self.rules = rules
-    
+
 
 class IntentTemplate(TemplateBase):
-    def __init__(self, intent: str, file_path = "", sentences: list[str] = []):
+    def __init__(self, intent: str, file_path="", sentences: list[str] = []):
         super().__init__(file_path)
         self.intent = intent
         self.sentences = sentences
-    
+
 
 class Intents(TemplateBase):
-    def __init__(self, file_path = "", intents: dict[str, IntentTemplate] = {}):
+    def __init__(self, file_path="", intents: dict[str, IntentTemplate] = {}):
         super().__init__(file_path)
         self.intents = intents
-    
+
 
 class Templates(TemplateBase):
     def __init__(
         self,
-        file_path = "",
+        file_path="",
         lists: dict[str, ListTemplate] = {},
         expansion_rules: dict[str, ExpansionRuleTemplate] = {},
         intents: dict[str, IntentTemplate] = {},
-        skip_words: list[str] = []
+        skip_words: list[str] = [],
     ):
         super().__init__(file_path)
         self.lists = lists
@@ -93,12 +97,14 @@ class Templates(TemplateBase):
 
 
 class ParseResult(Generic[TTemplateOrDict]):
-    def __init__(self, file_path: str = "", errors: list[str] = [], templates: TTemplateOrDict = None):
+    def __init__(
+        self, file_path: str = "", errors: list[str] = [], templates: TTemplateOrDict = None
+    ):
         self.file_path = file_path
         self.errors = errors
         self.templates = templates
 
-    
+
 TEMPLATE_DIR = path.join(path.dirname(__file__), "templates")
 
 
@@ -106,7 +112,8 @@ def load_yaml_file(file_path: str) -> dict:
     with open(file_path, "r", encoding="utf-8") as file:
         return yaml.full_load(file)
 
-def parse_list_templates(file_path:str, data: dict) -> ParseResult[dict[str, ListTemplate]]:
+
+def parse_list_templates(file_path: str, data: dict) -> ParseResult[dict[str, ListTemplate]]:
     result = ParseResult[dict[str, ListTemplate]](file_path=file_path, templates={})
     for list_name, list_data in data.get("lists", {}).items():
         if "range" in list_data:
@@ -116,12 +123,19 @@ def parse_list_templates(file_path:str, data: dict) -> ParseResult[dict[str, Lis
                     file_path=file_path,
                     values=[
                         str(n * int(list_data["range"].get("multiplyer", 1)))
-                        for n 
-                        in range(
+                        for n in range(
                             int(list_data["range"]["from"]),
                             int(list_data["range"]["to"]) + 1,
-                            round((int(list_data["range"]["to"]) + 1 - int(list_data["range"]["from"])) * .33)
-                        )]
+                            round(
+                                (
+                                    int(list_data["range"]["to"])
+                                    + 1
+                                    - int(list_data["range"]["from"])
+                                )
+                                * 0.33
+                            ),
+                        )
+                    ],
                 )
             except Exception as e:
                 result.errors.append(f"Error parsing range for {list_name} in {file_path}: {e}")
@@ -132,9 +146,13 @@ def parse_list_templates(file_path:str, data: dict) -> ParseResult[dict[str, Lis
                     file_path=file_path,
                     id=list_name,
                     values=[
-                        item if isinstance(item, str) else (item["out"] if "out" in item else item["in"])
+                        (
+                            item
+                            if isinstance(item, str)
+                            else (item["out"] if "out" in item else item["in"])
+                        )
                         for i, item in enumerate(list_data["values"])
-                    ]
+                    ],
                 )
             except Exception as e:
                 result.errors.append(f"Error parsing values for {list_name} in {file_path}: {e}")
@@ -142,20 +160,26 @@ def parse_list_templates(file_path:str, data: dict) -> ParseResult[dict[str, Lis
         elif "wildcard" in list_data:
             pass
         else:
-            result.errors.append(f"Unknown list type for {list_name} in {file_path}: {json.dumps(list_data)}")
+            result.errors.append(
+                f"Unknown list type for {list_name} in {file_path}: {json.dumps(list_data)}"
+            )
     return result
 
-def parse_expansion_templates(file_path:str, data: dict) -> ParseResult[dict[str, ExpansionRuleTemplate]]:
+
+def parse_expansion_templates(
+    file_path: str, data: dict
+) -> ParseResult[dict[str, ExpansionRuleTemplate]]:
     result = ParseResult[dict[str, ExpansionRuleTemplate]](file_path=file_path, templates={})
     for rule_name, rule_data in data.get("expansion_rules", {}).items():
         result.templates[rule_name] = ExpansionRuleTemplate(
-            file_path=file_path,
-            id=rule_name,
-            rule=rule_data
+            file_path=file_path, id=rule_name, rule=rule_data
         )
     return result
 
-def parse_intent_templates(file_path:str, data: dict) -> (ParseResult[dict[str, IntentTemplate]], int):
+
+def parse_intent_templates(
+    file_path: str, data: dict
+) -> (ParseResult[dict[str, IntentTemplate]], int):
     sentence_count = 0
     result = ParseResult[dict[str, IntentTemplate]](file_path=file_path, templates={})
     for intent_name, intent_data in data.get("intents", {}).items():
@@ -164,7 +188,9 @@ def parse_intent_templates(file_path:str, data: dict) -> (ParseResult[dict[str, 
             try:
                 sentences_list.extend(item.get("sentences", []))
             except Exception as e:
-                result.errors.append(f"Error parsing sentences for intent {intent_name} in {file_path}: {e}")
+                result.errors.append(
+                    f"Error parsing sentences for intent {intent_name} in {file_path}: {e}"
+                )
                 traceback.print_exc()
         result.templates[intent_name] = IntentTemplate(
             file_path=file_path,
@@ -173,6 +199,7 @@ def parse_intent_templates(file_path:str, data: dict) -> (ParseResult[dict[str, 
         )
         sentence_count += len(sentences_list)
     return result, sentence_count
+
 
 def parse_yaml_file(file_path: str) -> ParseResult[Templates]:
     data = load_yaml_file(file_path)
@@ -185,15 +212,18 @@ def parse_yaml_file(file_path: str) -> ParseResult[Templates]:
 
     intents_result, sentence_count = parse_intent_templates(file_path, data)
     result.templates.intents = intents_result.templates
-    
-    result.templates.errors = lists_result.errors + expansion_rules_result.errors + intents_result.errors
+
+    result.templates.errors = (
+        lists_result.errors + expansion_rules_result.errors + intents_result.errors
+    )
     return result, sentence_count
+
 
 def load_all_templates(directory: str) -> (ParseResult[Templates], int):
     sentence_count = 0
     all_templates = Templates(file_path=directory)
     all_errors = []
-    
+
     for root, _, files in walk(directory):
         for file in files:
             if file.endswith(".yaml") or file.endswith(".yml"):
@@ -201,31 +231,51 @@ def load_all_templates(directory: str) -> (ParseResult[Templates], int):
                 result, file_sentences = parse_yaml_file(file_path)
                 sentence_count += file_sentences
                 duplicate_list_keys = all_templates.lists.keys() & result.templates.lists.keys()
-                duplicate_expansion_keys = all_templates.expansion_rules.keys() & result.templates.expansion_rules.keys()
+                duplicate_expansion_keys = (
+                    all_templates.expansion_rules.keys() & result.templates.expansion_rules.keys()
+                )
                 if duplicate_list_keys:
-                    all_errors.extend([
-                        f"Duplicate list keys {key} found in {all_templates.lists[key].file_path} and {result.templates.lists[key].file_path}"
-                        for key in duplicate_list_keys
-                    ])
+                    all_errors.extend(
+                        [
+                            f"Duplicate list keys {key} found in {all_templates.lists[key].file_path} and {result.templates.lists[key].file_path}"
+                            for key in duplicate_list_keys
+                        ]
+                    )
                 if duplicate_expansion_keys:
-                    all_errors.extend([
-                        f"Duplicate expansion keys {key} found in {all_templates.expansion_rules[key].file_path} and {result.templates.expansion_rules[key].file_path}"
-                        for key in duplicate_expansion_keys
-                    ])  
-                all_templates.lists.update({k: v for k, v in result.templates.lists.items() if k not in duplicate_list_keys})
-                all_templates.expansion_rules.update({k: v for k, v in result.templates.expansion_rules.items() if k not in duplicate_expansion_keys})
+                    all_errors.extend(
+                        [
+                            f"Duplicate expansion keys {key} found in {all_templates.expansion_rules[key].file_path} and {result.templates.expansion_rules[key].file_path}"
+                            for key in duplicate_expansion_keys
+                        ]
+                    )
+                all_templates.lists.update(
+                    {
+                        k: v
+                        for k, v in result.templates.lists.items()
+                        if k not in duplicate_list_keys
+                    }
+                )
+                all_templates.expansion_rules.update(
+                    {
+                        k: v
+                        for k, v in result.templates.expansion_rules.items()
+                        if k not in duplicate_expansion_keys
+                    }
+                )
                 for intent, intent_data in result.templates.intents.items():
                     if intent not in all_templates.intents:
-                        all_templates.intents[intent] = IntentTemplate(file_path="", intent=intent, sentences=[])
+                        all_templates.intents[intent] = IntentTemplate(
+                            file_path="", intent=intent, sentences=[]
+                        )
                     all_templates.intents[intent].sentences.extend(intent_data.sentences)
 
                 all_errors.extend(result.errors)
-    
-    return ParseResult[Templates](
-        file_path=directory,
-        errors=all_errors,
-        templates=all_templates
-    ), sentence_count
+
+    return (
+        ParseResult[Templates](file_path=directory, errors=all_errors, templates=all_templates),
+        sentence_count,
+    )
+
 
 SPECIAL_CHARS = set("[({<")
 
@@ -320,11 +370,7 @@ def _expand_cached(expr, rules_id, lists_id):
                 i += 1
             tokens.append((expr[start:i],))
 
-    return tuple(
-        s for s in
-        (p.strip() for p in _combine(tokens))
-        if s
-    )
+    return tuple(s for s in (p.strip() for p in _combine(tokens)) if s)
 
 
 _RULES = {}
@@ -361,10 +407,7 @@ def generate_sentences_parallel(
         return generate_sentences(expr, rules, prepped)
 
     with ProcessPoolExecutor(max_workers=max_workers) as pool:
-        parts = pool.map(
-            lambda b: expand(b, rules, prepped),
-            branches
-        )
+        parts = pool.map(lambda b: expand(b, rules, prepped), branches)
 
     out = set()
     for p in parts:
@@ -438,8 +481,8 @@ def generate_sentences_streaming(
         rec(0, "")
 
     stream_expr(expr, tuple(), emit)
-    
-    
+
+
 def get_route(intent: str) -> str:
     if intent.startswith("Hass"):
         return "CommandControl"
@@ -448,7 +491,8 @@ def get_route(intent: str) -> str:
     if intent in ["request-movie", "request-tv-show", "search-media"]:
         return "FetchMedia"
     return "Unknown"
-    
+
+
 if __name__ == "__main__":
     result, sentence_count = load_all_templates(TEMPLATE_DIR)
     if result.errors:
@@ -457,20 +501,26 @@ if __name__ == "__main__":
             print(f"- {error}")
     else:
         # print(json.dumps(to_dict(result.templates.intents["Query-Media"].data), indent=2))
-        rules={k: v["rule"] for k, v in to_dict(result.templates.expansion_rules).items()}
-        lists={k: v["values"] for k, v in to_dict(result.templates.lists).items()}
+        rules = {k: v["rule"] for k, v in to_dict(result.templates.expansion_rules).items()}
+        lists = {k: v["values"] for k, v in to_dict(result.templates.lists).items()}
         print(json.dumps(lists, indent=2))
         print("Templates loaded successfully.")
         progress = 0
         with open(path.join(__file__, "training.jsonl"), "w", encoding="utf-8") as f:
+
             def consume(sentence: str):
                 f.write(sentence + "\n")
+
             for intent, intent_data in result.templates.intents.items():
+
                 def format(sentence):
-                    return json.dumps({
-                        "text": sentence,
-                        "label": get_route(intent),
-                    })
+                    return json.dumps(
+                        {
+                            "text": sentence,
+                            "label": get_route(intent),
+                        }
+                    )
+
                 for sentence in intent_data.sentences:
                     generate_sentences_streaming(
                         sentence,
@@ -482,4 +532,3 @@ if __name__ == "__main__":
                     if progress % 5 == 0:
                         print(f"Processed {progress} out of {sentence_count} templates...\n")
                     progress += 1
-  
